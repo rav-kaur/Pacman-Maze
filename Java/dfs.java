5c9bf372a88690584306b2000245453c60c08ad7
Ravneet Kaur <kaur9100@mylaurier.ca>
2020-06-19 19:56:14 -0400

    added dfs and updated comments

--- Java/dfs.java
+++ Java/dfs.java
@@ -4,15 +4,15 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Queue;
 
 import java.util.*;
 
 
-/**This class contains functions that will parse through a maze and perform an Breadth-First Search algorithm to find the 
+/**This class contains functions that will parse through a maze and perform an Depth-First Search algorithm to find the 
  * a path from the start node to the end node
  */
-public class bfs {
+public class dfs {
+
     public static void main(String[]args) throws Exception {
         Maze m = new Maze();
         Scanner sc = new Scanner(System.in);
@@ -32,69 +32,62 @@ public class bfs {
         sc.close();
 
         // solves the maze using bfs algorithm
-        SolveMaze_BFS(m);
+        SolveMaze_DFS(m);
         m.PrintMaze();
 
     }
 
     /**
-     * Breath-First Search Algorithm that takes a Maze (by reference) in as a parameter 
-     * and uses the bfs algorithm to find a solution from the start node to end node
+     * Depth-First Search Algorithm that takes a Maze (by reference) in as a parameter 
+     * and uses the dfs algorithm to find a solution from the start node to end node
      * @param m - Maze that needs to be solved
      */
-    public static void SolveMaze_BFS(Maze m)
+    public static void SolveMaze_DFS(Maze m)
 	{
         // keeps track of the path that is explored in bfs algorithm
         Hashtable <Node, Node> path = new Hashtable <Node, Node>();
+        
+        Node start = m.GetStart();
 
-        Node start = m.GetStart(); // start node
-
-        Queue<Node> frontier = new LinkedList<>(); // keeps track of future nodes to explore
+        Stack<Node> nodeStack = new Stack<>(); // keeps track of future nodes to explore
         Set<Node> explored = new HashSet<Node>(); // keeps track of nodes already explored
 
-        frontier.add(start);
-
-        // loops while frontier is not empty
-        while(!frontier.isEmpty()) {
-
-            Node current = frontier.remove();
+        nodeStack.add(start);
+        while(!nodeStack.isEmpty()) {
+            Node current = nodeStack.pop();
 
             if (!current.IsVisited()){
-
+                
                 /**
                  * if the current node is the end node -- it will start to rebuild path
                  * using backtracking and a hastable named path which keeps track of the 
                  * path explored
                  */
-
                 if (current.GetNodeType() == Node.NodeType.end){
                     System.out.println("Explored Nodes: "+ explored.size());
                     rebuildPath(current, path);
-                } 
-                else {
+                } else {
                     
+                    explored.add(current);
                     // finds all 4 neighbors of the current node
-                    ArrayList<Node> neighbours = m.getNeighbours(current, current.getCol(), current.getRow());
-
-                    for (Node neighbour : neighbours){
+                    ArrayList<Node> neighbors = m.getNeighbours(current, current.getCol(), current.getRow());
+                    for (Node neighbor : neighbors){
                         // only adds if it's not already explored and if it's not a wall
-                        if (!explored.contains(neighbour) && !neighbour.IsVisited() && neighbour.GetNodeType() != Node.NodeType.wall){
-                            frontier.add(neighbour);
-                            path.put(neighbour, current);
+                        if (!explored.contains(neighbor) && neighbor.GetNodeType() != Node.NodeType.wall){
+                            nodeStack.add(neighbor);
+                            path.put(neighbor, current);
                         }
                     }
                     current.SetVisited(true); // sets the current to visited so it's not checked again
-                    explored.add(current);
                 } 
             }
         }
-        
     }
 
     /**
      * This function back tracks and rebuilds the path from the end node to the start node
      * @param current - the end of the maze
-     * @param path - keeps track of the path that solveMaze_BFS follows (node, parent)
+     * @param path - keeps track of the path that solveMaze_DFS follows (node, parent)
      */
     public static void rebuildPath(Node current, Hashtable<Node, Node> path){
         LinkedList<Node> finalPath = new LinkedList<>();
